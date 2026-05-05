b52b3fdd9be48ce69a89beedb41e5f3c0ed12131
Luz Mendoza Alcantara <luzmmalcantara@gmail.com>
2026-05-04 23:54:46 -0300

    update Proyects component

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -3,15 +3,23 @@ import styles from "../css/Proyects.module.css"
 import adoptaImg from "../assets/proyectarbol.png"
 import hugExpress from "../assets/proyectpersonas.png"
 import vitalink from "../assets/proyectcase.png"
+import pasteleria from "../assets/proyectpasteleria.png"
 
 const Proyects=()=>{
     const projects= [
         {
-            title: "Adopta Tu Arbol",
-            image: adoptaImg,
-            description: "Proyecto academico frontend que representa una plataforma de adopción de árboles. Implementa funcionalidades interactivas como filtrado por tipo de árbol, cambio de estado al adoptar, y simulación de donaciones con mensajes de confirmación. El objetivo fue practicar manipulación del DOM, eventos y lógica en JavaScript, junto con una estructura visual clara y responsive.",
-            github: "https://github.com/LuzMendozaAlcantara/AdoptaTuArbol_.git",
-            demo: "https://adopta-tu-arbol-1lwz.vercel.app/"
+            title: "Pagina web VITALINK",
+            image: vitalink,
+            description: "Proyecto grupal de desarrollo web sobre primeros auxilios presentado a empresas de la red de la Fundacion Forge. Me trabajo fue la gestión de la base de datos de cada caso de emergencia mediante Postman; el diseño y maquetado de las cards en la página principal y la creación de vistas individuales interactivas para cada caso, garantizando una experiencia de usuario sencilla y de facil acceso y un diseño visual consistente.",
+            github: "https://github.com/LuzMendozaAlcantara/Vitalink",
+            demo: "https://grupo4-proyecto-final-client.vercel.app/"
+        },
+        {
+            title: "Rosa Bakery",
+            image: pasteleria,
+            description: "Desarrollé una página web dinámica y atractiva para un emprendimiento real de pastelería. El objetivo principal fue crear una experiencia súper amigable para que los clientes pudieran tentarse con los postres y armar su pedido de forma rápida y sencilla, sin que la página se trabe o tenga que recargar. ",
+            github:"https://github.com/LuzMendozaAlcantara/RosaBakery",
+            demo: "https://rosa-bakery.vercel.app/"
         },
         {
             title: "Hug Express",
@@ -19,13 +27,6 @@ const Proyects=()=>{
             description: "Desarrolle una interfaz web llamada Hug Express, una plataforma simulada para enviar abrazos a amigos y familiares. El proyecto incluia: listado dinamico de amigos con informacion personalizada, boton funcional de 'Enviar abrazo' que incrementa el contador de abrazos por usuario, implementacion de alertas interactivas, simulacion de gestion de cookies, incluyendo mensaje de consentimiento y boton para aceptar. Es un diseño estructurado tipo wireframe enfocado en experiencia del usuario.",
             github: "https://github.com/LuzMendozaAlcantara/HugExpress",
             demo: "https://hug-express.vercel.app/#"
-        },
-        {
-            title: "Pagina web VITALINK",
-            image: vitalink,
-            description: "Proyecto academico final grupal de desarrollo web sobre primeros auxilios presentado a empresas de la red de la Fundacion Forge. Me trabajo fue la gestión de la base de datos de cada caso de emergencia mediante Postman; el diseño y maquetado de las cards en la página principal y la creación de vistas individuales interactivas para cada caso, garantizando una experiencia de usuario sencilla y de facil acceso y un diseño visual consistente.",
-            github: "https://github.com/LuzMendozaAlcantara/Vitalink",
-            demo: "https://grupo4-proyecto-final-client.vercel.app/"
         }
     ];
 
